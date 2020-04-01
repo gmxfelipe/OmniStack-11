8b388fdfb6d05b37e84cff3ca94800e13c09a5c6
unknown <felipeAgomes>
2020-04-01 20:13:21 -0300

    Mobile concluído

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 10,
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  incident: {
+    marginTop: 10,
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    marginBottom: 8,
+  },
+
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold',
+    marginTop: 24,
+  },
+
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: '#737380'
+  },
+
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    marginBottom: 8,
+  },
+
+  heroTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#13131a',
+    lineHeight: 30,
+  },
+
+  heroDescription: {
+    fontSize: 15,
+    color: '#737380',
+    marginTop: 16,
+  },
+
+  actions: {
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  action: {
+    backgroundColor: '#e02041',
+    borderRadius: 8,
+    height: 50,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  actionText: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold',
+  }
+});
