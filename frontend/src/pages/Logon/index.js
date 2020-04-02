41a7d7400f2f7f59fd76d0c1893c04220778afa2
unknown <felipeAgomes>
2020-04-02 19:15:48 -0300

    Contrução de interface Web finalizada

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -36,7 +36,7 @@ import heroesImg from '../../assets/heroes.png'
                  <img src={logoImg} alt="Be The Hero" />
 
                     <form onSubmit={handleLogin}>
-                        <h1>Faça seu logon</h1>
+                        <h1>Faça seu login</h1>
 
                         <input 
                         placeholder="Sua Id"
